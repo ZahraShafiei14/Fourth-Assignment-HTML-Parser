b7a05d81e7122d9fc636f858ab933ca827084bb4
Zahra <zahrashafiei14@gmail.com>
2024-04-24 06:03:00 +0330

    Completed country and parser classes

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,45 +4,61 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class Parser {
     static List<Country> countries = new ArrayList<>();
 
     public List<Country> sortByName(){
         List<Country> sortedByName = new ArrayList<>(countries);
-        // Sort countries alphabetically (least)
-        //TODO
-        return  sortedByName;
+        sortedByName.sort(Comparator.comparing(Country::getName));
+        return sortedByName;
     }
 
     public List<Country> sortByPopulation(){
         List<Country> sortedByPopulation = new ArrayList<>(countries);
-        // Sort countries by population (most)
-        //TODO
+        sortedByPopulation.sort(Comparator.comparingInt(Country::getPopulation).reversed());
         return sortedByPopulation;
     }
 
     public List<Country> sortByArea(){
         List<Country> sortedByArea = new ArrayList<>(countries);
-        // Sort countries by area (most)
-        //TODO
+        sortedByArea.sort(Comparator.comparingDouble(Country::getArea).reversed());
         return sortedByArea;
     }
 
     public void setUp() throws IOException {
+        File input = new File("src/Resources/country-list.html");
+        Document doc = Jsoup.parse(input,"UTF-8");
 
-        //Parse the HTML file using Jsoup
-        //TODO
-
-        // Extract data from the HTML
-        //TODO
-
-        // Iterate through each country div to extract country data
-        //TODO
+        Elements countryElements = doc.select("div.country");
+        for (Element countryElement : countryElements) {
+            String name = countryElement.select(".country-name").text();
+            String capital = countryElement.select(".country-capital").text();
+            int population = Integer.parseInt(countryElement.select(".country-population").text());
+            double area = Double.parseDouble(countryElement.select(".country-area").text());
+            Country country = new Country(name, capital, population, area);
+            countries.add(country);
+        }
     }
 
     public static void main(String[] args) {
-        //you can test your code here before you run the unit tests ;)
+            Parser parser = new Parser();
+            try { parser.setUp(); } catch (IOException exception) {}
+
+            System.out.println("Countries sorted by: \n(1) Area\n(2) Name\n(3) Population");
+            Scanner scanner = new Scanner(System.in);
+            List<Country> countryList = new ArrayList<>(countries);
+            switch (scanner.nextInt()) {
+                case 1: countryList = parser.sortByArea(); break;
+                case 2: countryList = parser.sortByName(); break;
+                case 3: countryList = parser.sortByPopulation(); break;
+            }
+            for (Country country : countryList) {
+                System.out.println("-"+country.toString());
+            }
     }
 }
